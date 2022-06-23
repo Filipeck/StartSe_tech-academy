64609d43bfe08ebbb1b50a2694e16b84206991a7
Filipe Cayres <contato.fcayres@gmail.com>
2022-06-22 21:33:17 -0300

    DOM Js initial

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+const listaTarefas = document.querySelector('#listaTarefas');
+const caixaTexto = document.querySelector('#caixaDeTexto');
+const botaoAdicionar = document.querySelector('#botaoAdicionar');
+
+botaoAdicionar.addEventListener('click', function(){
+    
+});
