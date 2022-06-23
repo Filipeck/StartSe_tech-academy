87ccae8b6a4fc7b743e0f536427d3788d75bb918
Filipe Cayres <contato.fcayres@gmail.com>
2022-06-22 22:08:20 -0300

    list add/remove itens

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,5 +3,35 @@ const caixaTexto = document.querySelector('#caixaDeTexto');
 const botaoAdicionar = document.querySelector('#botaoAdicionar');
 
 botaoAdicionar.addEventListener('click', function(){
-    
+    const textoDaTarefa = caixaTexto.value;
+    caixaTexto.value = '';
+
+    listaTarefas.appendChild(adicionaTarefa(textoDaTarefa));
+    caixaTexto.focus();
 });
+
+function adicionaTarefa(textoDaTarefa){
+    const elementoLI = document.createElement('li');
+    const elementoSPAN = document.createElement('span');
+
+    elementoSPAN.setAttribute('id','tarefa');
+    elementoSPAN.textContent = textoDaTarefa;
+
+    elementoLI.appendChild(elementoSPAN);
+    elementoLI.appendChild(adicionaBotaoRemover());
+
+    return elementoLI;
+}
+
+function adicionaBotaoRemover(){
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = '✖';
+    botaoRemover.className = 'remover';
+
+    botaoRemover.addEventListener('click', function(){
+        listaTarefas.removeChild(this.parentNode); //aqui definimos que queremos remover o nó pai do button, o li no qual ele se encontra.
+       }
+    );
+
+    return botaoRemover;
+}
